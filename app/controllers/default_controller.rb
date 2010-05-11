990ce4a36b3a725bd83a634c13e6e06a80c610b1
Joe Dollard <jdollard@gmail.com>
2010-05-11 12:19:07 -0700

    resolve caching issue

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,38 +1,16 @@
 class DefaultController < ApplicationController
-  #list any automatically generated files here (they cannot be cached)
-  NO_CACHE = []
-  
-  #http redirects
-  #REDIRECTS = [{:orig => 'orig.html', :rewrite_as => '/new.html'}]
-  REDIRECTS = []
-  
   def index
     path = params[:path].to_s
     
-    if redirect = REDIRECTS.find {|r| path.include?(r[:orig]) }
-      redirect_to redirect[:rewrite_as]
-    elsif template_exists? s_path = 'static/' + path
-      render_cached s_path
+    if template_exists?(s_path = 'static/' + path)
+      render :template => s_path
     else
       raise ::ActionController::RoutingError,
             "Recognition failed for #{request.path.inspect}"
     end
   end
   
-  private
-    def render_cached(path)
-      if NO_CACHE.find {|nc| path.include?(nc) }
-        render :template => path
-      else
-        key = path.gsub('/', '-')
-        unless content = read_fragment(key)
-          content = render_to_string :template => path, :layout => false
-          write_fragment(key, content)
-        end
-        render :text => content, :layout => true
-      end
-    end
-    
+  private    
     # HACK: figure out a way not to have to do this dodgy shite
     # Define template_exists? for Rails 2.3 (cause it's deprecated)
     unless ActionController::Base.private_instance_methods.include? 'template_exists?'
@@ -41,5 +19,5 @@ class DefaultController < ApplicationController
       rescue ActionView::MissingTemplate
         false
       end
-    end    
+    end
 end
